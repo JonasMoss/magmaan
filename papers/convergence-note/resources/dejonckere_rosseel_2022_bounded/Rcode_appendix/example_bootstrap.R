74db5a1427a6f6480bac99b280c753a4ba8d4749
Jonas Moss <jonas.gjertsen@gmail.com>
2026-05-23 09:09:26 +0200

    Stop tracking convergence note paper files

--- papers/convergence-note/resources/dejonckere_rosseel_2022_bounded/Rcode_appendix/example_bootstrap.R
+++
@@ -1,6 +0,0 @@
-# fit the model with wide bounds + boostrap
-set.seed(1234)
-fit.bootstrap <- sem(model, data = Data, estimator = "ML",
-                     bounds = "wide", verbose = TRUE,
-                     se = "bootstrap", bootstrap = 1000L)
-summary(fit.bootstrap)
